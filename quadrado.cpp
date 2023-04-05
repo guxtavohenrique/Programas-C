da0ecaec95a24c28baa6a40243665117ad48660d
Gustavo Henrique <gustavosobralmagalhaes@gmail.com>
2023-04-05 10:16:41 -0300

    Add files via upload

---
+++ quadrado.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+	int lado_1, lado_2, area;
+	
+	cout<<"Digite o primeiro lado: ";
+	cin>>lado_1;
+	
+	cout<<"Digite o segundo lado: ";
+	cin>>lado_2;
+	
+	area = lado_1 * lado_2;
+	
+	cout<<"A area do quadrado e igaul a: ";
+	cout<<area;
+}
+
