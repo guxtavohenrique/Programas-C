3ced729038b0dd471a3fc4cda93b0fc279977acf
Gustavo Henrique <gustavosobralmagalhaes@gmail.com>
2023-04-05 10:12:24 -0300

    Add files via upload
    Utilizando printf e scanf.

--- area do quadrado.cpp
+++ area do quadrado.cpp
@@ -1,15 +1,20 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+
 int main (void)
 {
-	int lado_1, lado_2, area;
+	int lado1, lado2, area;
 	
-	cout<<"Digite o primeiro lado: ";
-	cin>>lado_1;
-	cout<<"Digite o segundo lado: ";
-	cin>>lado_2;
-	area = lado_1 * lado_2;
-	cout<<"A area do quadrado e igaul a: ";
-	cout<<area;
+	printf("Digite o primeiro lado do quadrado: ");
+	scanf("%d", &lado1);
+	
+	printf("Digite o segundo lado do quadrado: ");
+	scanf("%d", &lado2);
+	
+	area = lado1 * lado2;
+	
+	printf("A area do quadrado sera de: %d\n", area);
+	
+	system ("pause");
+	return(0);
 }
-
